3315301ae09a22bac6057c92ee8eecb17e7dbb62
Sahadat20 <49335257+Sahadat20@users.noreply.github.com>
2024-05-09 16:35:30 +0600

    Update views.py

--- polls/views.py
+++ polls/views.py
@@ -3,5 +3,5 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello! My name is Sahadat Hossain")
+    return HttpResponse("Hello! My name is Sahadat Hossain. THis is second commit")
 
